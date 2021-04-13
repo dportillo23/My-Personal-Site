26262dbe4a44a5d3f54a76b439b9a58b95f2c947
Daniel Portillo <depr.daniel@gmail.com>
2021-04-13 20:11:18 +0200

    Some mobile screen adjustments

--- Js/app.js
+++ Js/app.js
@@ -1,7 +1,46 @@
+const contactBtn = document.getElementById("contactBtn");
+
+function checkOrientation () {
+    if (window.matchMedia("(orientation: portrait)").matches)
+    {
+    
+        contactBtn.classList.remove("nav__btn", "btn");
+        contactBtn.classList.add("nav__link");
+    
+    } 
+    if (window.matchMedia("(orientation: landscape)").matches)
+    {
+        contactBtn.classList.add("nav__btn", "btn");
+        contactBtn.classList.remove("nav__link")
+    }
+}
+
+checkOrientation()
+window.addEventListener('resize', checkOrientation)
+
+
+//  Hamburger
+
 const hamburger = document.querySelector(".hamburger")
+const navLinks = document.querySelector(".nav__list")
+const links = document.querySelectorAll(".nav__item")
 
 hamburger.addEventListener("click", () =>{
-    hamburger.classList.toggle("change")
+    hamburger.classList.toggle("change");
+    navLinks.classList.toggle("open");
+    links.forEach(link => {
+        link.classList.toggle('fade')
+    })
+    links.forEach(link => {
+        link.addEventListener("click", (e) => {
+            console.log(e)
+            hamburger.classList.toggle("change");
+            navLinks.classList.toggle("open");
+            links.forEach(link => {
+            link.classList.toggle('fade')
+    })
+        })
+    })
 })
 
 
